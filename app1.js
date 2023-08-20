e6c784c5d3c489b9f51cff116b08ad89dc928af7
Anuj Kumar <67889993+anuj8052@users.noreply.github.com>
2023-08-21 04:06:59 +0530

    added app1.js file

---
+++ app1.js
@@ -0,0 +1,19 @@
+const http = require('http')
+
+const express = require('express');
+
+// console.log(routes.someText);
+const app = express();
+
+app.use((req, res, next) =>{
+    console.log("we are in the middleware");
+    next(); // allows to request to continue to the next middleware in the line
+});
+
+app.use((req, res, next) =>{
+    console.log("we are in the middleware agian");
+});
+
+const server = http.createServer(app);
+
+server.listen(1000);
